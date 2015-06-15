7ead3de921434480167b1c3ad123f5b2f94f410f
hbenbrahim <hbenbrahim@webuildmedia.com>
2015-06-15 14:54:49 +0000

    Added easy scrolling

--- index.php
+++ index.php
@@ -163,5 +163,6 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script src="js/scrolling-nav.js"></script>
+  <script src="js/jquery-easing.min.js"></script>
 </body>
 </html>
