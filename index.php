36fd6601028fb3b31ad032dd1b8679c35e955513
Hamza Benbrahim <hbenbrahim@webuildmedia.com>
2015-06-21 12:45:05 +0000

    Added the recruit and poll sections in the right side, which took a little space from the articles, which is cool, looks better

--- index.php
+++ index.php
@@ -1,29 +1,29 @@
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <link rel="icon" href="img/favicon.ico">
-  <!-- Bootstrap core CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/scrolling-nav.css" rel="stylesheet">
-  
-  <!-- Custom styles for this template -->
-  <link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
-  <link href="css/style.css" rel="stylesheet">
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!--[if lt IE 9]>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="img/favicon.ico">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/scrolling-nav.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+    <link href="css/style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
-  </head>
+</head>
 
-  <body>
+<body>
 
     <!-- Navigation -->
     <nav class="navbar navbar-default navbar-fixed-top menu-animated" role="navigation">
@@ -35,7 +35,8 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="page-scroll" href="#page-top"><img width="200px" height="auto" src="img/logo.png"/></a>
+                <a class="page-scroll" href="#page-top"><img width="200px" height="auto" src="img/logo.png" />
+                </a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
@@ -49,7 +50,7 @@
                         <a class="page-scroll" href="actu.php">Actualités</a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="agenda.php">Agenda</a>
+                        <a class="page-scroll" href="http://capdema.ma">Capdema</a>
                     </li>
                     <li>
                         <a class="page-scroll" href="espace_doc.php">Documents</a>
@@ -62,107 +63,128 @@
     </nav>
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div id="page-top" class="jumbotron">
-      <div class="container">
-      	<h1>Fact-Checking</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur</p>
-        <p><a class="btn btn-animated" href="#" role="button">Nos Articles &raquo;</a></p>
-      </div>
+        <div class="container">
+            <h1>Fact-Checking</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur</p>
+            <p><a class="btn btn-animated" href="#" role="button">Nos Articles &raquo;</a>
+            </p>
+        </div>
     </div>
+    <div class="row">
+       <h1>Test</h1>
+        <div class="container">
+            <hr class="featurette-divider">
+            <div class="col-md-9">
+                <!-- START THE FEATURETTES -->
 
+                <div class="row featurette">
+                    <div class="col-md-7">
+                        <h2 class="featurette-heading">Lorem <span class="text-muted">Ipsum.</span></h2>
+                        <span class="author">Author Mcauthor </span> <span class="pull-right author">12/07/2015</span>
+                        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="featurette-image img-responsive center-block" src="img/placehold.gif" width="200px" height="200px" alt="Generic placeholder image">
+                    </div>
+                </div>
 
-    <div class="container">
-      <!-- START THE FEATURETTES -->
+                <hr class="featurette-divider">
 
-      <hr class="featurette-divider">
+                <div class="row featurette">
+                    <div class="col-md-7">
+                        <h2 class="featurette-heading">Lorem <span class="text-muted">Ipsum.</span></h2>
+                        <span class="author">Author Mcauthor </span> <span class="pull-right author"> 14/07/2015</span>
+                        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="featurette-image img-responsive center-block" src="img/placehold.gif" width="200px" height="200px" alt="Generic placeholder image">
+                    </div>
+                </div>
 
-      <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">Lorem <span class="text-muted">Ipsum.</span></h2>
-          <span class="author">Author Mcauthor </span> <span class="pull-right author">12/07/2015</span>
-          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block" src="img/placehold.gif" width="200px" height="200px" alt="Generic placeholder image">
-        </div>
-      </div>
+                <hr class="featurette-divider">
 
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">Lorem <span class="text-muted">Ipsum.</span></h2>
-          <span class="author">Author Mcauthor </span> <span class="pull-right author"> 14/07/2015</span>
-          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                <div class="row featurette">
+                    <div class="col-md-7">
+                        <h2 class="featurette-heading">Lorem Ipsum.</h2>
+                        <span class="author">Author Mcauthor </span> <span class="pull-right author"> 22/07/2015 </span>
+                        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="featurette-image img-responsive center-block" src="img/placehold.gif" width="200px" height="200px" alt="Generic placeholder image">
+                    </div>
+                </div>
+                <!-- /END THE FEATURETTES -->
+            </div>
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Poll</div>
+                    <div class="panel-body">
+                        Panel content
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Poll</div>
+                    <div class="panel-body">
+                        Panel content
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block" src="img/placehold.gif" width="200px" height="200px" alt="Generic placeholder image">
+        <!-- /container -->
+    </div>
+    <div class="newsletter">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Newsletter</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <p class="lead">Restez informés en vous inscrivant à la Newsletter. </p>
+                </div>
+                <div class="col-md-6">
+                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required>
+                    <a class="btn btn-animated" href="#" role="button">Valider &raquo;</a>
+                </div>
+            </div>
         </div>
-      </div>
-
-      <hr class="featurette-divider">
-
-      <div class="row featurette">
-        <div class="col-md-7">
-          <h2 class="featurette-heading">Lorem Ipsum.</h2>
-          <span class="author">Author Mcauthor </span> <span class="pull-right author"> 22/07/2015 </span>
-          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block" src="img/placehold.gif" width="200px" height="200px" alt="Generic placeholder image">
-        </div>
-      </div>
-        </div> <!-- /container -->  
-
-      <!-- /END THE FEATURETTES -->
-	  <div class="newsletter">
-	  	<div class="container">
-	  		<div class="row">
-	  	<div class="col-md-12">
-          <h1>Newsletter</h1>
-        </div>
-       </div>
-       <div class="row">
-	  	<div class="col-md-6">
-          <p class="lead">Restez informés en vous inscrivant à la Newsletter. </p>
-        </div>
-        <div class="col-md-6">
-          <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required>
-          <a class="btn btn-animated" href="#" role="button">Valider &raquo;</a>
-        </div>
-	  </div>
-	  	</div>
-	</div> <!-- ./ NEWSLETTER -->
-      <footer>
-      	<div class="container">
-      	<div class="row">
-      		<div class="col-md-12 copyright">&copy; Capdema 2015</div>
-      	</div>
-      		<div class="row">
-      			<div id="social_media_buttons" class="col-md-12 social"> 
-      			<a href="#" data-hover="F" class="social">
+    </div>
+    <!-- ./ NEWSLETTER -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 copyright">&copy; Capdema 2015</div>
+            </div>
+            <div class="row">
+                <div id="social_media_buttons" class="col-md-12 social">
+                    <a href="#" data-hover="F" class="social">
       				F
       			</a>
-      			<a href="#" data-hover="T" class="social">
+                    <a href="#" data-hover="T" class="social">
       				T
       			</a>
-      			<a href="#" data-hover="l" class="social">
+                    <a href="#" data-hover="l" class="social">
       				l
       			</a>
-      			<a href="#" data-hover="g" class="social">
+                    <a href="#" data-hover="g" class="social">
       				g
       			</a>
-      		</div>
-      		</div>
-      	</div>
-      </footer>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 
-  <!-- Placed at the end of the document so the pages load faster -->
-  <!-- Bootstrap core JavaScript
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- Bootstrap core JavaScript
     ================================================== -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/scrolling-nav.js"></script>
-  <script src="js/jquery-easing.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scrolling-nav.js"></script>
+    <script src="js/jquery-easing.min.js"></script>
 </body>
+
 </html>
